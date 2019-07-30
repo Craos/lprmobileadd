912f8e99719600ae676d6c2f0342ec97aac47b4a
gubleo <43252092+gubleo@users.noreply.github.com>
2019-07-30 15:21:32 -0300

    Versão Inicial do Sistema - 1.2

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,25 +1,2 @@
-var app = {
-    initialize: function(){
-        this.bindEvents();
-    },
-
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-        function capturePhoto(){
-            navigator.camera.getPicture(onSuccess, onFail, { quality: 25,
-                destinationType: Camera.DestinationType.DATA_URL
-            });
-
-            function onSuccess(imageData) {
-                let image = document.getElementById('minhaImagem');
-                image.src = "data:image/jpeg;base64," + imageData;
-            }
-
-            function onFail(message) {
-                alert('Failed because: ' + message);
-            }
-    }
-};
 
 app.initialize();
